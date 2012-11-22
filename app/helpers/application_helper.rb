1d07d0949baa97ab298a56fceb3810a5c0b527b8
koteshwerrao <kotesh.rao506@gmail.com>
2012-11-22 10:41:15 +0530

    Update app/helpers/application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,4 @@ module ApplicationHelper
   def download_url_for(file_filename)  
         AWS::S3::S3Object.url_for(file_filename,"em-ftpd-trial-kotesh", :authenticated => false)  
     end 
-
-def torrent_url_for(file_filename)  
-        download_url_for(file_filename) + "?torrent"  
-    end  
 end
