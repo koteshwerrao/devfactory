c4db874549ed47efcf4559f4a29333d2d2e9e70b
Koteshwar Rao R <koteshwarrao@heurion.com>
2012-11-29 11:56:24 +0530

    changes

--- lib/file_manager.rb
+++ lib/file_manager.rb
@@ -1,7 +1,7 @@
 require 'net/ftp'
 class FileManager
 
-	def self.ftp_connect
+ def self.ftp_connect
 		ftp =	Net::FTP.new
  		ftp.connect("0.0.0.0","2100")
  		ftp.login("admin", "test123")
@@ -12,7 +12,7 @@ class FileManager
 		dirs = []
 		files = []
 		frams.each do |f|
-			if f[0] == "d"
+			 if f[0] == "d"
 				dirs << f if f.split(/\s+/).last != ".."
 			else
 				files << f
